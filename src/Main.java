4b431885dc9333db48639f2ad6be975cdf3d9626
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-07 20:45:14 -0400

    3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int a;
-        int b;
-        int sum;
+//        int a;
+//        int b;
+//        int sum;
 
-        for (a = 1; a <= 12; ++a) {
-            for (b = 1; b <= 12; ++b) ;
-            sum = a * b;
+        for (int a = 1; a <= 12; a++) {
+            for (int b = 1; b <= 12; b++) ;
+//            sum = a * b;
             {
-                System.out.print(sum + " ");
+                System.out.print((a * b) + "\t");
             }
             System.out.println();
         }
